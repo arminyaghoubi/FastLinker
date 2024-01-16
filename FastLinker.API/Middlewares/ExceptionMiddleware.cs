0a252251eef6f810f3a1bb6ce6c60a79c795103d
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2024-01-16 12:06:52 +0330

    #45 Create Global Exception Handler

---
+++ FastLinker.API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,76 @@
+﻿using FastLinker.API.Models;
+using FastLinker.Application.Exceptions;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace FastLinker.API.Middlewares;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
+
+    public ExceptionMiddleware(RequestDelegate next,
+        ILogger<ExceptionMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await ExceptionHandlerAsync(context, ex);
+        }
+    }
+
+    private async Task ExceptionHandlerAsync(HttpContext context, Exception ex)
+    {
+        HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+        CustomValidationProblemDetails problem;
+
+        switch (ex)
+        {
+            case BadRequestException badRequestException:
+                statusCode = HttpStatusCode.BadRequest;
+                problem = new CustomValidationProblemDetails
+                {
+                    Title = badRequestException.Message,
+                    Detail = badRequestException.InnerException?.Message,
+                    Errors = badRequestException.ValidationErrors,
+                    Type = nameof(BadRequestException),
+                    Status = (int)statusCode
+                };
+                break;
+            case NotFoundException notFoundException:
+                statusCode = HttpStatusCode.NotFound;
+                problem = new CustomValidationProblemDetails
+                {
+                    Title = notFoundException.Message,
+                    Detail = notFoundException.InnerException?.Message,
+                    Type = nameof(NotFoundException),
+                    Status = (int)statusCode
+                };
+                break;
+            default:
+                problem = new CustomValidationProblemDetails
+                {
+                    Title = ex.Message,
+                    Detail = ex.InnerException?.Message,
+                    Type = nameof(HttpStatusCode.InternalServerError),
+                    Status = (int)statusCode
+                };
+                break;
+        }
+
+        context.Response.StatusCode = (int)statusCode;
+        var errorMessage = JsonConvert.SerializeObject(problem);
+        _logger.LogError(ex, errorMessage);
+        await context.Response.WriteAsJsonAsync(problem);
+    }
+}
--- FastLinker.API/Program.cs
+++ FastLinker.API/Program.cs
@@ -1,10 +1,16 @@
 using FastLinker.Infrastructure;
 using FastLinker.Persistence;
 using FastLinker.Application;
+using Serilog;
+using FastLinker.API.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Host.UseSerilog((context, configure) =>
+    configure.WriteTo.Console()
+    .ReadFrom.Configuration(context.Configuration));
+
 builder.Services.AddPersistenceServices(builder.Configuration)
     .AddInfrastructureServices()
     .AddApplicationServices();
@@ -16,12 +22,16 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
