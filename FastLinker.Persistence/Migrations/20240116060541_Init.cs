7c3fcc865aa2db4403bdb47eb18c1ad00dc69ab0
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2024-01-16 09:36:19 +0330

    #9 Add Migration

---
+++ FastLinker.Persistence/Migrations/20240116060541_Init.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FastLinker.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Links",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Url = table.Column<string>(type: "nvarchar(225)", maxLength: 225, nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreationDateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Links", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShortLinks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ShortKey = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: false),
+                    LinkId = table.Column<int>(type: "int", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreationDateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShortLinks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShortLinks_Links_LinkId",
+                        column: x => x.LinkId,
+                        principalTable: "Links",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clicks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ip = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    ShortLinkId = table.Column<int>(type: "int", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreationDateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clicks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Clicks_ShortLinks_ShortLinkId",
+                        column: x => x.ShortLinkId,
+                        principalTable: "ShortLinks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clicks_Ip_ShortLinkId",
+                table: "Clicks",
+                columns: new[] { "Ip", "ShortLinkId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clicks_ShortLinkId",
+                table: "Clicks",
+                column: "ShortLinkId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShortLinks_LinkId",
+                table: "ShortLinks",
+                column: "LinkId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clicks");
+
+            migrationBuilder.DropTable(
+                name: "ShortLinks");
+
+            migrationBuilder.DropTable(
+                name: "Links");
+        }
+    }
+}
