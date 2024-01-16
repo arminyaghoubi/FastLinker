6a3f4a075539f3fb08f84c53ead8e0e6bc8567da
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2024-01-16 10:01:13 +0330

    #17 Implement Validation CreateShortLinkCommand

---
+++ FastLinker.Application/Features/Shortener/Commands/CreateShortLinkCommandValidation.cs
@@ -0,0 +1,20 @@
+﻿using FastLinker.Application.Resources;
+using FluentValidation;
+
+namespace FastLinker.Application.Features.Shortener.Commands;
+
+public class CreateShortLinkCommandValidation : AbstractValidator<CreateShortLinkCommand>
+{
+    public CreateShortLinkCommandValidation()
+    {
+        RuleFor(c => c.Url)
+            .NotEmpty().WithMessage(ErrorMessageResource.EmptyOrNullUrl)
+            .NotNull().WithMessage(ErrorMessageResource.EmptyOrNullUrl)
+            .Must(ValidUrl).WithMessage(ErrorMessageResource.InvalidUrl);
+    }
+
+    private bool ValidUrl(string url)
+    {
+        return Uri.TryCreate(url, UriKind.Absolute, out _);
+    }
+}
