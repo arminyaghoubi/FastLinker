3da81e93acbed906102cd3fb949112aa66c2cb31
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2024-01-16 09:50:31 +0330

    #13 Create Command Shortener

---
+++ FastLinker.Application/Features/Shortener/Commands/CreateShortLinkCommand.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace FastLinker.Application.Features.Shortener.Commands;
+
+public class CreateShortLinkCommand : IRequest<string>
+{
+    public string? Title { get; set; }
+    public string Url { get; set; } = string.Empty;
+}
---
+++ FastLinker.Application/Features/Shortener/Commands/CreateShortLinkCommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace FastLinker.Application.Features.Shortener.Commands;
+
+public class CreateShortLinkCommandHandler : IRequestHandler<CreateShortLinkCommand, string>
+{
+    Task<string> IRequestHandler<CreateShortLinkCommand, string>.Handle(CreateShortLinkCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
