cd4c2f8b9edfe90144cb5cd895bffddf5eb0d5f5
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2024-01-16 09:26:58 +0330

    #5 Implement Configuration Classes for Domain Classes

---
+++ FastLinker.Persistence/Configurations/ClickConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using FastLinker.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FastLinker.Persistence.Configurations;
+
+public class ClickConfiguration : IEntityTypeConfiguration<Click>
+{
+    public void Configure(EntityTypeBuilder<Click> builder)
+    {
+        builder.Property(c => c.Ip)
+            .IsRequired()
+            .HasMaxLength(15);
+
+        builder.HasIndex(c => new { c.Ip, c.ShortLinkId }).IsUnique();
+    }
+}
---
+++ FastLinker.Persistence/Configurations/ShortLinkConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using FastLinker.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FastLinker.Persistence.Configurations;
+
+public class ShortLinkConfiguration : IEntityTypeConfiguration<ShortLink>
+{
+    public void Configure(EntityTypeBuilder<ShortLink> builder)
+    {
+        builder.Property(s => s.ShortKey)
+            .IsRequired()
+            .HasMaxLength(8);
+    }
+}
