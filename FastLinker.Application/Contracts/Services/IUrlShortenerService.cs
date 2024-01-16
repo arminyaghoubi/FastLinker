9e36564a89cf1cbc697e5bc8b077b704a480f736
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2024-01-16 10:18:54 +0330

    #21 Implement ShortLink CommandHandler

---
+++ FastLinker.Application/Contracts/Services/IUrlShortenerService.cs
@@ -0,0 +1,6 @@
+﻿namespace FastLinker.Application.Contracts.Services;
+
+public interface IUrlShortenerService
+{
+    string ShortenUrl(string originalUrl);
+}
--- FastLinker.Application/Features/Shortener/Commands/CreateShortLinkCommandHandler.cs
+++ FastLinker.Application/Features/Shortener/Commands/CreateShortLinkCommandHandler.cs
@@ -1,11 +1,78 @@
-﻿using MediatR;
+﻿using FastLinker.Application.Contracts.Persistence;
+using FastLinker.Application.Contracts.Services;
+using FastLinker.Application.Exceptions;
+using FastLinker.Application.Resources;
+using FastLinker.Domain;
+using MediatR;
 
 namespace FastLinker.Application.Features.Shortener.Commands;
 
 public class CreateShortLinkCommandHandler : IRequestHandler<CreateShortLinkCommand, string>
 {
-    Task<string> IRequestHandler<CreateShortLinkCommand, string>.Handle(CreateShortLinkCommand request, CancellationToken cancellationToken)
+    private readonly IShortLinkRepository _shortLinkRepository;
+    private readonly ILinkRepository _linkRepository;
+    private readonly IUrlShortenerService _shortenerService;
+
+    public CreateShortLinkCommandHandler(IShortLinkRepository shortLinkRepository,
+        ILinkRepository linkRepository,
+        IUrlShortenerService shortenerService)
     {
-        throw new NotImplementedException();
+        _shortLinkRepository = shortLinkRepository;
+        _linkRepository = linkRepository;
+        _shortenerService = shortenerService;
+    }
+
+    public async Task<string> Handle(CreateShortLinkCommand request, CancellationToken cancellationToken)
+    {
+        CreateShortLinkCommandValidation validation = new();
+        var validationResult = await validation.ValidateAsync(request, cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            throw new BadRequestException(ErrorMessageResource.BadRequest, validationResult);
+        }
+
+        var link = await _linkRepository.GetLinkByUrlAsync(request.Url);
+
+
+        if (link is null)
+        {
+            link = new Link
+            {
+                Url = request.Url,
+                IsActive = true
+            };
+            await _linkRepository.CreateLinkAsync(link);
+        }
+
+        if (request.Title is not null)
+        {
+            var shortLink = await _shortLinkRepository.GetShortLinkByTitleAsync(request.Title);
+
+            if (shortLink is null)
+            {
+                shortLink = new ShortLink
+                {
+                    Title = request.Title,
+                    LinkId = link.Id,
+                    ShortKey = _shortenerService.ShortenUrl(link.Url),
+                    IsActive = true
+                };
+                await _shortLinkRepository.CreateShortLinkAsync(shortLink);
+            }
+
+            return shortLink.ShortKey;
+        }
+        else
+        {
+            var shortLink = new ShortLink
+            {
+                LinkId = link.Id,
+                ShortKey = _shortenerService.ShortenUrl(link.Url),
+                IsActive = true
+            };
+            await _shortLinkRepository.CreateShortLinkAsync(shortLink);
+
+            return shortLink.ShortKey;
+        }
     }
 }
