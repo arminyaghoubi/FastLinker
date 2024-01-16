3e8ac19ada40ce60d163c2e83cf2d33f937d7446
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2024-01-16 10:45:19 +0330

    #31 Implement and Register UrlShortenerService

--- FastLinker.API/Program.cs
+++ FastLinker.API/Program.cs
@@ -1,9 +1,11 @@
+using FastLinker.Infrastructure;
 using FastLinker.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddPersistenceServices(builder.Configuration);
+builder.Services.AddPersistenceServices(builder.Configuration)
+    .AddInfrastructureServices();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
